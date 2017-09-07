6a1686c743ae4cb3959eafb0a13b70ae18c493c0
Nicholas Dille <nicholas.dille@haufe-lexware.com>
2017-09-07 21:39:35 +0200

    Added class for SVG based line charts

---
+++ ScriptChart/SvgChart/SvgLineChart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace SvgChart
+{
+    public class SvgLineChart : ILineChart
+    {
+        private XmlDocument _svg;
+
+        public SvgLineChart()
+        {
+            _svg = new XmlDocument();
+            //
+        }
+
+        public string CreateLineChart(List<Tuple<float, float>> points)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateLineChart(List<float> points)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
