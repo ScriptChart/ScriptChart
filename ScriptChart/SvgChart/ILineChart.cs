447e9dca90aeec2cae8f012a1780ec318e0f918a
Nicholas Dille <nicholas.dille@haufe-lexware.com>
2017-09-07 21:14:18 +0200

    Added interface for line chart

---
+++ ScriptChart/SvgChart/ILineChart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SvgChart
+{
+    interface ILineChart
+    {
+        string CreateLineChart(List<Tuple<float, float>> points);
+
+        string CreateLineChart(List<float> points);
+    }
+}
