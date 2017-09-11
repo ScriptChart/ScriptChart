60de084224984e6b698a7b2876fd32dcb9e2cb7b
Alexey Paramonov <alex.s.paramonov@gmail.com>
2017-09-12 00:30:47 +0200

    Implemented headers support. This closes #15

--- WebApi/Controllers/LineChartController.cs
+++ WebApi/Controllers/LineChartController.cs
@@ -9,12 +9,18 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using SvgChart;
+using System.IO;
+using Microsoft.Extensions.Primitives;
+using System.Dynamic;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class LineChartController : Controller
     {
+        IDataConverter DataConverter { get; set; } = new DataConverter();
+        ILineChart LineChart { get; set; } = new SvgLineChart();
+
         // GET api/linechart
         [HttpGet]
         public IEnumerable<string> Get()
@@ -31,14 +37,61 @@ namespace WebApi.Controllers
 
         // POST api/linechart
         [HttpPost]
-        public ContentResult Post([FromBody]JToken req)
+        public JsonResult Post()
         {
-            IDataConverter dataConverter = new DataConverter();
-            var data = dataConverter.ConvertData(req, "$..Id", "$..DownloadSpeedInBits");
-            ILineChart lineChart = new SvgLineChart();
-            string result = lineChart.CreateLineChart(data);
+            
+            string xAxisPath = string.Empty;
+            string yAxisPath = string.Empty;
+            string result = string.Empty;
 
-            return this.Content(result, "application/xml");
+            StringValues xJpathCollection;
+            StringValues yJpathCollection;
+
+            string body;
+            using (var stream = HttpContext.Request.Body)
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    body = sr.ReadToEnd();
+                }
+            }
+
+            JToken jtoken;
+            try
+            {
+                jtoken = JToken.Parse(body);
+            }
+            catch (Exception exception)
+            {
+                dynamic errorResult = new ExpandoObject();
+                errorResult.Error = exception.Message;
+                return Json(errorResult);
+            }
+
+            if (HttpContext.Request.Headers.TryGetValue("X-JPATH-FOR-Y", out yJpathCollection))
+            {
+                yAxisPath = yJpathCollection.FirstOrDefault();
+            }
+            else
+            {
+                throw new HttpRequestException("X-JPATH-FOR-Y header has not been found.");
+            }
+
+            if (HttpContext.Request.Headers.TryGetValue("X-JPATH-FOR-X", out xJpathCollection))
+            {
+                xAxisPath = xJpathCollection.FirstOrDefault();
+                var data = DataConverter.ConvertData(jtoken, xAxisPath, yAxisPath);
+                result = LineChart.CreateLineChart(data);
+            }
+            else
+            {
+                var data = DataConverter.ConvertData(jtoken, yAxisPath);
+                result = LineChart.CreateLineChart(data);
+            }
+
+            dynamic resultObj = new ExpandoObject();
+            resultObj.Svg = result;
+            return this.Json(resultObj);
         }
 
         // PUT api/linechart/5
