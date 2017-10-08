a4c379337044d33ed346151471ba9a41283f164e
Alexey Paramonov <alex.s.paramonov@gmail.com>
2017-10-08 12:04:36 +0200

    Added Key in model for chart object and removed cors. Closes #22

--- WebApi/Model/ChartCollectionDeserializer.cs
+++ WebApi/Model/ChartCollectionDeserializer.cs
@@ -1,10 +1,14 @@
-﻿namespace WebApi.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Model
 {
     public class ChartCollectionDeserializer
     {
-        public string _id { get; set; }
+     //   public string _id { get; set; }
         public float[][] Data { get; set; }
         public string Svg { get; set; }
+        [Key]
+        [Required]
         public string ChartId { get; set; }
     }
 }
